46425e50e263daebed43ec462f92defe797afa2c
gurucharankonamki13 <gurucharankonamki13@gmail.com>
2026-09-18 11:05:33 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,99 @@
+#Printing Messages & Basic print()
+
+print("Hello World!")
+print("Welcome to Python")
+print("Gurucharan")
+print("I am learning Python")
+print("Python Programming")
+print("Good Morning")
+print("Welcome Students")
+print("Python is easy")
+print("My first program")
+print("Hello from Python")
+
+#Quotes, Messages vs Calculations & Syntax
+
+print("2+5")
+print(2+5)
+print("10-3")
+print(10-3)
+print("Hello World!")
+print("Python")
+print("Hello")
+print("Hello")
+print("10+2")
+print(10+2)
+
+#Variables
+
+name = "Gururcharan"
+age = 20
+marks = 85
+price = 99.5
+city = "Chennai"
+print(city)
+score = 95
+print(score)
+x = 10
+print(x)
+message = "Welcome"
+print(message)
+a = 10 
+b = 20 
+print(a+b)
+quantity = 5
+price = 10 
+print(quantity * price)
+
+#Data Type: int,float, str
+
+int
+float
+str
+age = 21
+height = 5.8
+name = "Arun"
+age = 20
+price = 99.5
+name = "Ravi"
+int
+float
+str
+
+#Sequence of Insructions
+
+print("Start")
+print("Middle")
+print("End")
+#First
+#Secound
+#Third
+x = 10
+print(x)
+x = 20 
+print(x)
+number = 5
+print(number)
+number = 10 
+print(number)
+print("A")
+print("B")
+print("C")
+name = "Ravi"
+age = 20 
+print(name)
+print(age)
+print(1)
+print(2)
+print(3)
+print(4)
+x = 5
+y = 10
+print(x)
+print(y)
+print("Start")
+print(2+3)
+print("End")
+print("Login")
+print("Dashboard")
+print("Logout")
